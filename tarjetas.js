4a9775de6b5605e17359130add220b5a79815f7d
Norfolllk <alexander.garofalo.ag@gmail.com>
2026-04-27 01:33:59 -0500

    Reto Tarjetas VF segun Material Apoyo

--- tarjetas.js
+++ tarjetas.js
@@ -1,6 +1,13 @@
 function crearTarjetas() {
+    let desde = parseInt(txtDesde.value);
+    let hasta  = parseInt(txtHasta.value);
+    let salto = parseInt(txtSalto.value);
+
+    if (isNaN(salto)) {
+        salto = 1;
+    }
     let contenido = "";
-    for (let i = 1; i <= 5; i++) {
+    for (let i = desde; i <= hasta; i += salto) {
         contenido = contenido + "<div class='item'>" + i + "</div>";
         divTarjetas.innerHTML = contenido;
     }
