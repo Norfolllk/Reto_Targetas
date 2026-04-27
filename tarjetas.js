9281469ee9e423e6b5742ff46d88ec28d6491884
Norfolllk <alexander.garofalo.ag@gmail.com>
2026-04-27 01:10:32 -0500

    Reto Tarjetas Video Actualizacion

--- tarjetas.js
+++ tarjetas.js
@@ -1,1 +1,7 @@
-
+function crearTarjetas() {
+    let contenido = "";
+    for (let i = 1; i <= 5; i++) {
+        contenido = contenido + "<div class='item'>" + i + "</div>";
+        divTarjetas.innerHTML = contenido;
+    }
+}
